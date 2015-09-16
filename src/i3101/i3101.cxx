651181e1daacd3b3960dc69ffb3c24f45747cd35
Alexander Komarov <izard.ak@gmail.com>
2015-09-16 14:44:49 +0200

    Update i3101.cxx

--- src/i3101/i3101.cxx
+++ src/i3101/i3101.cxx
@@ -29,16 +29,52 @@
 using namespace upm;
 using namespace std;
 
-i3101::i3101(int cs, int write, int a[], int d[])
+i3101::i3101()
 {
-  if ( !(m_gpio = mraa_gpio_init(pin)) )
+  int i;
+  if ( !(m_gpio_cs = mraa_gpio_init(0)) )
     {
       cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
       return;
     }
 
-  mraa_gpio_dir(m_gpio, MRAA_GPIO_IN);
-  m_isrInstalled = false;
+  mraa_gpio_dir(m_gpio_cs, MRAA_GPIO_IN);
+  mraa_gpio_mode(m_gpio_cs, MRAA_GPIO_PULLUP); 
+  if ( !(m_gpio_write = mraa_gpio_init(1)) )
+    {
+      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      return;
+    }
+
+  mraa_gpio_dir(m_gpio_write, MRAA_GPIO_IN);
+  mraa_gpio_mode(m_gpio_write, MRAA_GPIO_PULLUP);
+  for (i = 0; i < 4; i++) {
+    if ( !(m_gpio_w[i] = mraa_gpio_init(i+2)) )
+    {
+      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      return;
+    }
+    mraa_gpio_dir(m_gpio_w[i], MRAA_GPIO_IN);
+    mraa_gpio_mode(m_gpio_w[i]], MRAA_GPIO_PULLUP);
+  for (i = 0; i < 4; i++) {
+    if ( !(m_gpio_a[i] = mraa_gpio_init(i+6)) )
+    {
+      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      return;
+    }
+    mraa_gpio_dir(m_gpio_a[i], MRAA_GPIO_IN);
+    mraa_gpio_mode(m_gpio_a[i]], MRAA_GPIO_PULLUP);
+  for (i = 0; i < 4; i++) {
+    if ( !(m_gpio_d[i] = mraa_gpio_init(i+10)) )
+    {
+      cerr << __FUNCTION__ << ": mraa_gpio_init() failed" << endl;
+      return;
+    }
+    mraa_gpio_dir(m_gpio_d[i], MRAA_GPIO_OUT);
+    mraa_gpio_mode(m_gpio_d[i]], MRAA_GPIO_PULLUP);
+  }
+  mraa_gpio_write(m_gpio_cs, 1);
+  mraa_gpio_write(m_gpio_write, 1);
 }
 
 i3101::~i3101()
@@ -48,10 +84,21 @@ i3101::~i3101()
 
 int i3101::read(int addr)
 {
-
+  int i;
+  int d[8];
+  if(addr < 0 || addr > 3) return -1;
+  mraa_gpio_write(m_gpio_cs, 0);
+  mraa_gpio_write(m_gpio_write, 1);
+  
+  for (i = 0; i < 4 i++)
+    mraa_gpio_write(m_gpio_a[i], (addr & (1<<i))>>i);
+  for (i = 0; i < 4 i++)
+    d[i] = mraa_gpio_read(m_gpio_d[i]);
 }
 
 int i3101::write(int addr, int value)
 {
+  if(addr < 0 || addr > 3) return -1;
+  if(value < 0 || value > 15) return -1;
 
 }
